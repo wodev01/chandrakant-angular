4afa1adeb7e986e6eeb5d21762d9e30d081fdfc3
wodev01 <wo.dev01@gmail.com>
2016-03-29 13:50:46 +0530

    Employees Form UI : Add the tab(Ui-grid) with different functionality

--- src/app/employees/employees.controller.js
+++ src/app/employees/employees.controller.js
@@ -6,7 +6,7 @@
         .controller('EmployeesController', EmployeesController);
 
     /** @ngInject */
-    function EmployeesController(EmployeesService,toastr,modalService) {
+    function EmployeesController(EmployeesService,toastr,modalService,uiGridGroupingConstants,uiGridConstants) {
 
         var vm = this;
 
@@ -15,10 +15,6 @@
             actionButtonText: 'Delete Employee',
             headerText: 'Delete ?',
             bodyText: 'Are you sure you want to delete this Employee?'
-        };
-
-        vm.fnListEmployees = function(){
-            vm.employees = EmployeesService.list();
         };
 
         vm.fnRemoveEmployee = function(id){
@@ -31,7 +27,59 @@
                         toastr.error("Employee Deleted getting problem!!");
                     });
             });
-        }
+        };
+
+        /*--- START : Employees UI Grid ---*/
+
+
+
+        vm.empGridOption = {
+            enableFiltering : true,
+//            showGridFooter: true,
+            showColumnFooter : true,
+            enableRowSelection: true,
+            enableRowHeaderSelection: false,
+            rowHeight:50,
+            paginationPageSizes: [5,10, 15, 20],
+            enablePaginationControls: true,
+            paginationPageSize: 5
+        };
+
+        vm.empGridOption.columnDefs = [
+            {
+                name: 'picture',
+                displayName: 'Picture',
+                cellTemplate : '<div class="ngCellText ui-grid-cell-contents ng-binding ng-scope image-wrapper"><img src="{{COL_FIELD}}" " alt="" height="30" width="30"></div>',
+                width : '8%',
+                enableFiltering : false
+            },
+            { name: 'name', displayName: 'Name'},
+            { name: 'age', displayName: 'Age',width: '5%'},
+            { name: 'gender', displayName: 'Gender',width: '7%',enableFiltering : false},
+            { name: 'email', displayName: 'Email',enableFiltering : false},
+            { name: 'registered', displayName: 'Register Date',type: 'date',cellFilter: 'date:\'yyyy-MM-dd\'',enableFiltering : false},
+            { name: 'balance', displayName: 'Balance',aggregationType: uiGridConstants.aggregationTypes.sum,
+                treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+                customTreeAggregationFinalizerFn: function( aggregation ) {
+                    aggregation.rendered = aggregation.value;
+                },
+                enableFiltering : false
+            }
+        ];
+
+        vm.empGridOption.multiSelect = false;
+        vm.empGridOption.modifierKeysToMultiSelect = false;
+        vm.empGridOption.noUnselect = true;
+        vm.empGridOption.onRegisterApi = function( gridApi ) {
+            vm.gridApi = gridApi;
+        };
+
+        /*--- END : Employees UI Grid ---*/
+
+        vm.fnListEmployees = function(){
+            vm.employees = EmployeesService.list();
+            vm.empGridOption.data = vm.employees;
+        };
 
         vm.fnInitEmployees = function(){
             vm.fnListEmployees();
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,7 +2,11 @@
   'use strict';
 
   angular
-    .module('myTaskDemo', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'toastr','ui.bootstrap'])
+    .module('myTaskDemo', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource',
+          'ui.router', 'toastr','ui.bootstrap','ui.grid','ui.grid.autoResize',
+          'ui.grid.moveColumns','ui.grid.pinning','ui.grid.resizeColumns',
+          'ui.grid.edit','ui.grid.pagination','ui.grid.grouping','ui.grid.selection'])
+
       .filter('TotalBalanceFilter', function () {
           return function (input, property) {
               var i = input instanceof Array ? input.length : 0;
